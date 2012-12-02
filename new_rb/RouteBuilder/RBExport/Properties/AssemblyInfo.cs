d9f19309e6df14945bf1dc96c45288ebbc19947e
oleg5538@yandex.ru <oleg5538@yandex.ru@1ab0f0a6-0016-3eb4-19f5-9909de63748b>
2012-12-02 14:48:00 +0000

    The whole new version would be based on the DLL API instead of writing the whole code into EXE.

---
+++ new_rb/RouteBuilder/RBExport/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RouteBuilder API")]
+[assembly: AssemblyDescription("Application programming interface for RouteBuilder")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("UralSOFT")]
+[assembly: AssemblyProduct("RBAPI")]
+[assembly: AssemblyCopyright("Copyright © UralSOFT 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2bb967b3-98be-4264-86e2-038467f95caf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
