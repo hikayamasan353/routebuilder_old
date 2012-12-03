40bf54cf2a2e571f0fe3eecbdf7258cd0e043bf9
oleg5538@yandex.ru <oleg5538@yandex.ru@1ab0f0a6-0016-3eb4-19f5-9909de63748b>
2012-12-03 17:50:51 +0000

    uploading updated source code

---
+++ trunk/new_rb/RouteBuilder/RouteBuilder/FormExport.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RouteBuilder
+{
+    public class FormExport : Form
+    {
+        public FormExport()
+        {
+            InitializeComponent();
+        }
+
+        private void FormExport_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExportBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+    }
+}
